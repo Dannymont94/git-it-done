5f11545c8c87c9d758ec405a15c572778cb0a8eb
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-05 22:32:06 -0400

    added error handling for fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -15,14 +15,27 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        }); 
-    });
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                }); 
+            } else {
+                repoSearchTerm.textContent = "(Error: " + response.statusText + ")";
+            }
+        })
+        // this .catch() method gets chained to the end of the .then() method
+        .catch(function(error) {
+            repoSearchTerm.textContent = "(Unable to connect to GitHub)";
+        });
 };
 
 var displayRepos = function(repos, searchTerm) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
     for (var i = 0; i < repos.length; i++) {
