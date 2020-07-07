5b48c6e71b75f16de96d29f4b204cb5a2928ad13
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-07 15:47:57 -0400

    added search by topic buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -75,4 +76,32 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+        // repoContainerEl.textContent = "";
+    }   
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data.items, language);
+                });
+            } else {
+                repoSearchTerm.textContent = "(Error: " + response.statusText + ")";
+            }
+        })
+        .catch(function(error) {
+            repoSearchTerm.textContent = "(Unable to connect to GitHub)";
+        });
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
